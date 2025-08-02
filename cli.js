c075e60fa65bf8e1aa5c7e353824d3937c8ce38e
Thalyta Ferreira <thalyta_ferreira08@hotmail.com>
2025-08-02 15:32:35 -0300

    Arquivo concluido adicionado

--- cli.js
+++ cli.js
@@ -1,1 +1,7 @@
-//projeto CLI - Desenvolve s
+//projeto CLI - Desenvolve
+function piadas(categoria){
+  return fetch (`https://v2.jokeapi.dev/joke/${categoria}?lang=pt`)
+  .then(resposta => resposta.json())
+}
+
+piadas('any').then(dados =>{console.log(dados.setup, dados.delivery);})
